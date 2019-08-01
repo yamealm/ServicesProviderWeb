964a2082a402a19ec5fe8727c242792f6ac95a23
yamealm <yamealm@gmail.com>
2019-07-31 21:22:48 -0400

    modificar clientes

--- src/com/alodiga/services/provider/commons/ejbs/CustomerEJB.java
+++ src/com/alodiga/services/provider/commons/ejbs/CustomerEJB.java
@@ -36,7 +36,7 @@ public interface CustomerEJB extends SPGenericEJB {
 
     public Address saveAddress(Address address) throws NullParameterException, GeneralException;
 
-    public List<Customer> searchCustomers(Long enterpriseId, String login, String fullName, String email) throws EmptyListException, NullParameterException, GeneralException;
+    public List<Customer> searchCustomers(String login, String fullName, String email) throws EmptyListException, NullParameterException, GeneralException;
 
    
 }
